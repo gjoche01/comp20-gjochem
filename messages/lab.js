31b6c8c58de2c8326734c82b4522f2bc516ff8d0
gjoche01 <greta.jochem@tufts.edu>
2016-03-08 15:05:28 -0500

    did part 1

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,29 @@
-// Your JavaScript goes here...
+function parse(){
+
+	//First, make the request
+ 	var myRequest = new XMLHttpRequest();
+ 	myRequest.open("GET", "data.json", true);
+
+ 	console.log("testI"); 
+ 	
+ 	//now, handle the request
+ 	myRequest.onreadystatechange = function (){
+
+ 		//data must be ready 
+ 		if (myRequest.readyState == 4) {
+ 			result=""; 
+ 			raw = myRequest.responseText;
+ 			theData = JSON.parse(raw);
+ 			elem = document.getElementById("messages");
+
+ 		for(i=0; i<theData.length;  i++){
+ 				result += "<p>"+ theData[i]["username"] + " " + theData[i]["message"] +"</p>"; 
+
+ 			}
+ 			elem.innerHTML = result; 
+ 		}
+ 	}
+
+ 	myRequest.send(null); 
+}
+
